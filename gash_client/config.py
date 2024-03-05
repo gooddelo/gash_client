2fffa44571ef608ff9a6c510c9be6fb5fa325ff4
max <m.frolov.2003@gmail.com>
2024-03-05 12:58:59 +0400

    Move Config to conftest

--- gash_client/config.py
+++ gash_client/config.py
@@ -1,25 +1,6 @@
 from faststream.rabbit import RabbitExchange, RabbitQueue
-from faststream.rabbit.shared.utils import build_url
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from yarl import URL
 
 
-class AMQPConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix='RABBITMQ_')
-
-    host: str = 'localhost'
-    port: int = '5672'
-    default_user: str = 'admin'
-    default_pass: str = 'password'
-    vhost: str = '/'
-
-    def connection_url(self) -> URL:
-        return build_url(
-            host=self.host, port=self.port, login=self.default_user, password=self.default_pass, virtualhost=self.vhost
-        )
-
-
-AMQP_CONFIG = AMQPConfig()
 GASH_EXCHANGE = RabbitExchange("GASH", auto_delete=True)
 
 
