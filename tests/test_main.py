a1e8ff24f5d6bff3482fc08f1e9531a91a6505ba
max <m.frolov.2003@gmail.com>
2024-03-05 13:12:10 +0400

    Add test to update user.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,22 @@ from faststream.rabbit import RabbitBroker
 
 from tests.conftest import AMQP_CONFIG
 from gash_client.core.client import GashClient
-from gash_client.core.user.dto import UserCreateDTO
+from gash_client.core.user.dto import UserCreateDTO, UserUpdateDTO
 
 
 @pytest.mark.asyncio
-async def test_main():
+async def test_create_user():
     broker = RabbitBroker(AMQP_CONFIG.connection_url())
     await broker.connect()
     client = GashClient(broker)
     await client.user_create(UserCreateDTO(id_=f'{uuid4()}', role='client'))
+    await broker.close()
+
+
+@pytest.mark.asyncio
+async def test_update_user():
+    broker = RabbitBroker(AMQP_CONFIG.connection_url())
+    await broker.connect()
+    client = GashClient(broker)
+    await client.user_update(UserUpdateDTO(id_=f'{uuid4()}', old_role='client', new_role='client'))
     await broker.close()
